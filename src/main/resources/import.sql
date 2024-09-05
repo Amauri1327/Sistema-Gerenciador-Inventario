1523bc9f894052021ac5057f0c6ebe300a4807ad
Amauri M Ferreira <amauri816@hotmail.com>
2024-09-05 18:43:24 -0300

    Criacao Client Entity

---
+++ src/main/resources/import.sql
@@ -0,0 +1,1 @@
+INSERT INTO tb_client (name, cpf, address, phone) VALUES ('Amauri Ferreira', '123.456.789.00', 'Rua Brasil', '79999628750');
---
+++ src/test/java/com/SGInventario/invetario/SistemaInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SGInventario.invetario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
