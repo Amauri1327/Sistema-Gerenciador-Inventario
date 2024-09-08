d13744c55bf192ccf603b73d93e55b8fd2e6fd21
Amauri M Ferreira <amauri816@hotmail.com>
2024-09-08 12:59:53 -0300

    CRUD completo em Product

--- src/main/java/com/SGInventario/invetario/resources/ProductResource.java
+++ src/main/java/com/SGInventario/invetario/resources/ProductResource.java
@@ -1,13 +1,20 @@
 package com.SGInventario.invetario.resources;
 
 import java.io.Serializable;
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.SGInventario.invetario.dto.ProductDto;
 import com.SGInventario.invetario.services.ProductService;
@@ -26,5 +33,33 @@ public class ProductResource implements Serializable{
 		List<ProductDto> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<ProductDto> findById(@PathVariable Long id){
+		ProductDto dto = service.findById(id);
+		return ResponseEntity.ok().body(dto);
+	}
+	
+	@PostMapping 
+	public ResponseEntity<ProductDto> insert(@RequestBody ProductDto dto){
+		dto = service.insert(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(dto.id())
+				.toUri();
+		return ResponseEntity.created(uri).body(dto);
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<ProductDto> update(@PathVariable Long id, @RequestBody ProductDto dto){
+		dto = service.update(id, dto);
+		return ResponseEntity.ok().build();
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable long id){
+		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
 }
--- src/main/java/com/SGInventario/invetario/services/ProductService.java
+++ src/main/java/com/SGInventario/invetario/services/ProductService.java
@@ -2,6 +2,7 @@ package com.SGInventario.invetario.services;
 
 import java.io.Serializable;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,6 +11,9 @@ import org.springframework.stereotype.Service;
 import com.SGInventario.invetario.dto.ProductDto;
 import com.SGInventario.invetario.entities.Product;
 import com.SGInventario.invetario.repositories.ProductRepository;
+
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.transaction.Transactional;
 
 @Service
 public class ProductService implements Serializable{
@@ -22,6 +26,53 @@ public class ProductService implements Serializable{
 	public List<ProductDto> findAll(){
 		List<Product> entity = repo.findAll();
 		return entity.stream().map(ProductDto::new).collect(Collectors.toList());
+	}
+
+	public ProductDto findById(Long id) {
+		Optional<Product> obj = repo.findById(id);
+		Product entity = obj.orElseThrow(() -> new RuntimeException("Id not found"));
+		return new ProductDto(entity);
+	}
+	
+	@Transactional
+	public ProductDto insert(ProductDto dto) {
+		Product prod = new Product();
+		prod.setName(dto.name());
+		prod.setDescription(dto.description());
+		prod.setQuantity(dto.quantity());
+		prod.setCategory(dto.category());
+		prod.setMaxStock(dto.maxStock());
+		prod.setMinStock(dto.minStock());
+		repo.save(prod);
+		return new ProductDto(prod);
+	}
+
+	@Transactional
+	public ProductDto update(Long id, ProductDto dto) {
+		try {
+			Product prod = repo.getReferenceById(id);
+			prod.setName(dto.name());
+			prod.setDescription(dto.description());
+			prod.setQuantity(dto.quantity());
+			prod.setCategory(dto.category());
+			prod.setMaxStock(dto.maxStock());
+			prod.setMinStock(dto.minStock());
+			repo.save(prod);
+			return new ProductDto(prod);
+		}catch (EntityNotFoundException e) {
+			throw new EntityNotFoundException("Id not found: " + id);
+		}
+	}
+	
+	@Transactional
+	public void delete(long id) {
+		try {
+			repo.deleteById(id);
+		}
+		catch (EntityNotFoundException e) {
+			throw new EntityNotFoundException("Id not found: " + id);
+		}
+		
 	}
 	
 	
