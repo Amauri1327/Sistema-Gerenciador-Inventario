cdbfea4917a6aaeee8f2fe344c0f9ee621da73f2
Amauri M Ferreira <amauri816@hotmail.com>
2024-09-08 11:03:47 -0300

    Criacao Product Entity e seeding inicial

---
+++ src/main/java/com/SGInventario/invetario/entities/Product.java
@@ -0,0 +1,38 @@
+package com.SGInventario.invetario.entities;
+
+import java.io.Serializable;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@Entity
+@Table(name="tb_product")
+public class Product implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private Long id;
+	private String name;
+	private String description;
+	private Integer quantity;
+	private String category;
+	private Integer maxStock;
+	private Integer minStock;
+
+}
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -4,3 +4,9 @@ INSERT INTO tb_client (name, cpf, address, phone) VALUES ('Debora Pereira', '123
 INSERT INTO tb_supplier (name, cnpj, address, phone) VALUES ('Fasouto', '15.598.527/0008-03', 'Av. Simião Sobral', '(79) 2107-5454')
 INSERT INTO tb_supplier (name, cnpj, address, phone) VALUES ('TechSol', '47.252.912/0001-50', 'Rua Paulo Falcão, 152', '(11) 3273-8920');
 INSERT INTO tb_supplier (name, cnpj, address, phone) VALUES ('Comercial Guarujá', '32.458.781/0003-17', 'Avenida das Palmeiras, 2120', '(21) 3456-7789');
+
+
+INSERT INTO tb_product (name, description, quantity, category, max_Stock, min_Stock) VALUES ('Galaxy M35', 'Smartphone Samsung vendido online', 3, 'Eletronicos', 10, 1);
+INSERT INTO tb_product (name, description, quantity, category, max_Stock, min_Stock) VALUES ('iPhone 14', 'Smartphone Apple com câmera avançada', 5, 'Eletronicos', 15, 2);
+INSERT INTO tb_product (name, description, quantity, category, max_Stock, min_Stock) VALUES ('Dell XPS 13', 'Notebook ultrafino com tela infinita', 8, 'Computadores', 20, 3);
+INSERT INTO tb_product (name, description, quantity, category, max_Stock, min_Stock) VALUES ('Sony WH-1000XM4', 'Fone de ouvido com cancelamento de ruído', 12, 'Acessorios', 25, 5);
